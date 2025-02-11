405b9621ce822321e122e26ac5f43cf99034c820
funda-metal <zachencinas@gmail.com>
2025-02-11 16:02:39 -0500

    added character dictionary

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+#def main():
+    #with open("books/frankenstein.txt") as f:
+     #   file_contents = f.read()
+     #   words = file_contents.split()
+     #   print(f"{len(words)}")
+
+
+#main()
+
 def main():
+    char_dict = {}
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        words = file_contents.split()
-        print(f"{len(words)}")
-
+        lowercase = file_contents.lower()
+        chars = list(lowercase)
+        for char in chars:
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+    print char_dict
+            
+        
+        
 main()
+
